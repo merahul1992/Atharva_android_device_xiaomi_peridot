e58abd06e57f19ef8fe2aecaf381016a322073c1
merahul1992 <90108415+merahul1992@users.noreply.github.com>
2024-10-08 10:41:42 +0530

    Update and rename mist_peridot.mk to halcyon_peridot.mk

--- halcyon_peridot.mk
+++ halcyon_peridot.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common RisingOS stuff.
-$(call inherit-product, vendor/mist/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)
 
-PRODUCT_NAME := mist_peridot
+PRODUCT_NAME := halcyon_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
